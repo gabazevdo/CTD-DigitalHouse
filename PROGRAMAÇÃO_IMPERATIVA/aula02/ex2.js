b01c55637ba0c6f501e549bec784af71cc86ae7f
Gabazevdo <gbldja@gmail.com>
2021-10-25 21:09:51 -0300

    Exercicios feitos em aula

---
+++ PROGRAMAÇÃO_IMPERATIVA/aula02/ex2.js
@@ -0,0 +1,36 @@
+/* Crie um arquivo chamado ex2.js
+
+Declare, atribua e exiba 3 variáveis ​​por console: uma string, uma numérica e uma booleana. O código abaixo é apenas um exemplo. Desta forma, você pode criar as variáveis baseadas em outras situações:
+
+let nome = "Danilo"
+let idade = 25
+let usuarioAtivo = true
+ 
+console.log(nome)
+console.log(idade)
+console.log(usuarioAtivo)
+
+
+Como seria mostrar um texto que informa na tela o que é cada coisa? Observe o exemplo abaixo, mas crie o seu próprio:
+
+let nome = "Danilo"
+let idade = 25
+let usuarioAtivo = true
+ 
+console.log("Nome: ")
+console.log(nome)
+console.log("Idade: ")
+console.log(idade)
+console.log("Usuário está ativo no sistema?")
+console.log(usuarioAtivo)
+
+ */
+
+let nome = "Gabriel"
+let idade = 30
+let usuarioAtivo = true
+
+
+console.log("Nome: ", nome)
+console.log("Idade: ", idade)
+console.log("Usuário está ativo no sistema? ", usuarioAtivo)
