8f35d3b490a3519372eabd89968ffc0d106e6240
Gabazevdo <gbldja@gmail.com>
2021-10-30 23:24:38 -0300

    Exercicios desafios 1 ao 3 - resta o 4

--- PROGRAMAÇÃO_IMPERATIVA/aula04/calculadora.js
+++ PROGRAMAÇÃO_IMPERATIVA/aula04/calculadora.js
@@ -8,39 +8,38 @@ Crie uma função de multiplicação, que deverá receber dois parâmetros e ret
 Crie uma função de divisão, que receberá dois parâmetros e retornará o resultado da divisão do primeiro sobre o segundo.
  */
 
-console.log ("-------------- Teste de Operações / Calculadora / Desafio 1--------------")
+console.log(
+  "-------------- Teste de Operações / Calculadora / Desafio 1--------------"
+);
 
 // Adição
 const adicionar = (x, y) => {
-    return console.log(`
-A operação entre ${x} + ${y} é ${x+y}
-    `)
-}
-adicionar(5,5)
+  return console.log(`
+A operação entre ${x} + ${y} é ${x + y}
+    `);
+};
+adicionar(5, 5);
 
 //Subtração
 const subtrair = (x, y) => {
-    return console.log(`A operação entre ${x} - ${y} é ${x-y}
-    `)
-}
-subtrair(15,5)
+  return console.log(`A operação entre ${x} - ${y} é ${x - y}
+    `);
+};
+subtrair(15, 5);
 
 //Multiplicação
-const multiplicar = (x,y) => {
-    return console.log(`A operação entre ${x} * ${y} é ${x*y}
-    `)
-
-}
-multiplicar(4,2)
+const multiplicar = (x, y) => {
+  return console.log(`A operação entre ${x} * ${y} é ${x * y}
+    `);
+};
+multiplicar(4, 2);
 
 //Divisão
-const dividir = (x,y) =>{
-    return console.log(`A operação entre ${x} / ${y} é ${x/y}
-    `)
-}
-dividir(10,2)
-
-
+const dividir = (x, y) => {
+  return console.log(`A operação entre ${x} / ${y} é ${x / y}
+    `);
+};
+dividir(10, 2);
 
 /* DESAFIO ===> 
 Após passar o Nível I, adicionamos um pouco mais de dificuldade para testarmos nossas funções.
@@ -52,30 +51,62 @@ Execute a função que faz divisão, passando quaisquer dois números como argum
 Execute a função que faz divisão, passando agora o número zero como um dos dois argumentos. Mostre o resultado no console.
  */
 
-console.log ("-------------- Teste de Operações / Calculadora / Desafio 2--------------")
+console.log(
+  "-------------- Teste de Operações / Calculadora / Desafio 2--------------"
+);
 
-adicionar(50,5)
-subtrair(50,5)
-multiplicar(5,5)
-dividir(8,2)
-dividir(0,2)
-
+adicionar(50, 5);
+subtrair(50, 5);
+multiplicar(5, 5);
+dividir(8, 2);
+dividir(0, 2);
 
 /*  ====> DESAFIO 3
-Crie uma função chamada quadradoDoNumero, que recebe um número como parâmetro e deve retornar esse número multiplicado por ele mesmo, ou seja, ao quadrado.
+1. Crie uma função chamada quadradoDoNumero, que recebe um número como parâmetro e deve retornar esse número multiplicado por ele mesmo, ou seja, ao quadrado.
 Importante: quadradoDoNumero() deve usar a função multiplicação() para calcular o quadrado do parâmetro inserido em power().
 
-Crie a função mediaDeTresNumeros, ela deve calcular a média de 3 números, que serão inseridos por parâmetro.
+2. Crie a função mediaDeTresNumeros, ela deve calcular a média de 3 números, que serão inseridos por parâmetro.
 Importante: Em mediaDeTresNumeros() você precisará usar algumas funções criadas anteriormente em nossa calculadora.
 
 
-Crie a função calculaPorcentagem, que receberá dois parâmetros: o número total e a porcentagem que deseja calcular, e que deverá retornar x% de totalPorcentagem.
+3. Crie a função calculaPorcentagem, que receberá dois parâmetros: o número total e a porcentagem que deseja calcular, e que deverá retornar x% de totalPorcentagem.
 Exemplo: calculaPorcentagem(300, 15)  (deve retornar 45, pois é 15% de 300). 
 
 Importante: calculaPorcentagem() você precisará usar algumas funções criadas anteriormente em nossa calculadora.
 
 
-Crie uma função geradorDePorcentagem que leva dois parâmetros, e que deverá retornar a porcentagem do primeiro em relação ao segundo parâmetro.
+4. Crie uma função geradorDePorcentagem que leva dois parâmetros, e que deverá retornar a porcentagem do primeiro em relação ao segundo parâmetro.
 Exemplo: geradorDePorcentagem (2, 200) (deve retornar 1 já que 2 é 1% de 200).
  */
 
+console.log(
+  "-------------- Teste de Operações / Calculadora / Desafio 3--------------\n"
+);
+
+//Desafio 1
+const quadradoDoNumero = (numero) => {
+  return `Desafio 1 - O quadrado do número é: ${numero ** 2}
+    `;
+};
+
+const multiplicação = (power) => {
+  return power;
+};
+
+const power = () => {
+  return 10;
+};
+
+console.log(quadradoDoNumero(multiplicação(power())));
+
+//Desafio 2
+const mediaDeTresNumeros = (nota1, nota2, nota3) => {
+  return `Desafio 2 - A média é: ${(nota1 + nota2 + nota3) / 3}
+    `;
+};
+console.log(mediaDeTresNumeros(10, 10, 5));
+
+//Desafio 3
+const calculaPorcentagem = (valor, porcentagem) =>{
+     
+}
