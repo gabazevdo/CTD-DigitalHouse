3f4e57d58982760390a5bcbf1b7d17990435a20b
Gabazevdo <gbldja@gmail.com>
2021-11-11 21:32:04 -0300

    Modificacao para entrega atividade

--- PROGRAMAÇÃO_IMPERATIVA/aula09 - CHECKPOINT/microondas.js
+++ PROGRAMAÇÃO_IMPERATIVA/aula09 - CHECKPOINT/microondas.js
@@ -1,6 +1,7 @@
 // Início programa
 console.log(
-  `     |--------------.:: MICROONDAS ::.---------------|
+  `     
+        |--------------.:: MICROONDAS ::.---------------|
         _________________________________________________
         | Opção |  Alimento  | Tempo preparo |          |
         -------------------------------------------------
@@ -14,75 +15,63 @@ console.log(
 );
 
 const microondas = (opcao, tempo) => {
+  console.log(`Opção ${opcao} com o tempo de preparo em ${tempo}s`);
   if (opcao == 1) {
     if (tempo < 10) {
-      console.log(`${tempo}s -> Tempo insuficiente!`);
+      console.log(`Tempo insuficiente!`);
     } else if (tempo == 10 || tempo <= 19) {
-      console.log(`
-        Pipoca feita em ${tempo}s. 
-        Prato pronto, bom apetite!!! - Cuidado está quente`);
+      console.log(`Está pronto, bom apetite!!! - Cuidado, pode estar quente`);
     } else if (tempo == 20 || tempo <= 29) {
-      console.log(`${tempo}s  -> Passou do ideal - Queimou`);
+      console.log(`Passou do ideal - Queimou`);
     } else if (tempo == 30 || tempo > 30) {
-      console.log(`${tempo}s -> Passou de mais - KABUMMMM`);
+      console.log(`Eita, colocou muito tempo - KABUMMMM`);
     }
   } else if (opcao == 2) {
     if (tempo < 8) {
-      console.log(`${tempo}s -> Tempo insuficiente!`);
+      console.log(`Tempo insuficiente!`);
     } else if (tempo == 8 || tempo <= 16) {
-      console.log(`
-      Macarrão feito em ${tempo}s. 
-      Prato pronto, bom apetite!!! - Cuidado está quente`);
+      console.log(`Está pronto, bom apetite!!! - Cuidado, pode estar quente`);
     } else if (tempo == 17 || tempo <= 23) {
-      console.log("Queimou");
+      console.log(`Passou do ideal - Queimou`);
     } else if (tempo >= 24) {
-      console.log(`${tempo}s -> Passou de mais - KABUMMMM`);
+      console.log(`Eita, colocou muito tempo - KABUMMMM`);
     }
   } else if (opcao == 3) {
     if (tempo < 15) {
-      console.log(`${tempo}s -> Tempo insuficiente!`);
+      console.log(`Tempo insuficiente!`);
     } else if (tempo == 15 || tempo <= 29) {
-      console.log(`
-      Carne feita em ${tempo}s. 
-      Prato pronto, bom apetite!!! - Cuidado está quente`);
+      console.log(`Está pronto, bom apetite!!! - Cuidado, pode estar quente`);
     } else if (tempo == 30 || tempo <= 44) {
-      console.log(`${tempo}s  -> Passou do ideal - Queimou`);
+      console.log(`Passou do ideal - Queimou`);
     } else if (tempo >= 45) {
-      console.log(`${tempo}s -> Passou de mais - KABUMMMM`);
+      console.log(`Eita, colocou muito tempo - KABUMMMM`);
     }
   } else if (opcao == 4) {
     if (tempo < 12) {
-      console.log(`${tempo}s -> Tempo insuficiente!`);
+      console.log(`Tempo insuficiente!`);
     } else if (tempo == 12 || tempo <= 24) {
-      console.log(`
-      Feijão feito em ${tempo}s. 
-      Prato pronto, bom apetite!!! - Cuidado está quente`);
+      console.log(`Está pronto, bom apetite!!! - Cuidado, pode estar quente`);
     } else if (tempo == 25 || tempo <= 35) {
-      console.log(`${tempo}s  -> Passou do ideal - Queimou`);
+      console.log(`Passou do ideal - Queimou`);
     } else if (tempo >= 36) {
-      console.log(`${tempo}s -> Passou de mais - KABUMMMM`);
+      console.log(`Eita, colocou muito tempo - KABUMMMM`);
     }
   } else if (opcao == 5) {
     if (tempo < 8) {
-      console.log(`${tempo}s -> Tempo insuficiente!`);
+      console.log(`Tempo insuficiente!`);
     } else if (tempo == 8 || tempo <= 16) {
-      console.log(`        
-      Brigadeiro feito em ${tempo}s. 
-      Prato pronto, bom apetite!!! - Cuidado está quente`);
+      console.log(`Está pronto, bom apetite!!! - Cuidado, pode estar quente`);
     } else if (tempo == 17 || tempo <= 23) {
-      console.log(`${tempo}s  -> Passou do ideal - Queimou`);
+      console.log(`Passou do ideal - Queimou`);
     } else if (tempo >= 24) {
-      console.log(`${tempo}s -> Passou de mais - KABUMMMM`);
+      console.log(`Eita, colocou muito tempo - KABUMMMM`);
     }
   } else {
     console.log("Prato inexistente");
   }
 };
+//1º parâmetro - opção do prato (o ideial é entre 1 e 5)
+//2º parâmtreo - tempo do preparo
+microondas("pipoca", 89);
 
-
-//1º parâmetro - Passar a entre 1 à 5 
-//2º parâmtreo - Numeros a vontade 
-
-// Deixar o terminal expandido 
 // Fiz tbm utilizando o prompt =>  https://checkpoint1-microondas.netlify.app/
-microondas(1, 10);
