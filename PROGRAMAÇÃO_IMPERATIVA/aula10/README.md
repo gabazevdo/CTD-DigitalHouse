aaea70a1dd5370a3ae0c30e2d6a85c7a7b111852
Gabazevdo <gbldja@gmail.com>
2021-11-16 00:23:40 -0300

    Atividade aula 10 finalizada

---
+++ PROGRAMAÇÃO_IMPERATIVA/aula10/README.md
@@ -0,0 +1,18 @@
+<h1>Arrays e Coleções</h2>
+
+<ul>
+<li> Aula 10 </li>
+</ul>
+<div>
+<p>Vamos fortalecer nosso conhecimento sobre arrays explorando diferentes ações. Veremos se eles se comportam como esperamos ou se ocorrem resultados inesperados.
+Em primeiro lugar, vamos praticar as ações mais comuns e necessárias:</p>
+
+<ol>
+<li>Acessar elementos específicos de um array.</li>
+<li>Modificar cada um dos elementos de um array e imprimi-los no console.</li>
+<li>Adicionar elementos a um array.</li>
+<li>Extrair elementos de um array.</li>
+<li>Comparar elementos de um array com os elementos de outro. </li>
+</ol>
+
+</div>
--- PROGRAMAÇÃO_IMPERATIVA/aula10/ex01.js
+++ PROGRAMAÇÃO_IMPERATIVA/aula10/ex01.js
@@ -1,1 +1,54 @@
-// https://docs.google.com/document/d/1wUMuBww0N6Zk_ckgHL1hLBFVryrqFpRoK6Iom1LKzAE/edit#heading=h.dqwa0t8yrq0e
+let arrayString = ["maça", "banana", "abacaxi", "manga"]
+let arrayNumeros = [1,2,3,4]
+
+//Imprimindo arrays originais
+console.log(`Array de strings original: ${arrayString}`)
+console.log(`Array de números original: ${arrayNumeros}
+`)
+
+//1 - Imprimindo um item específico dos arrays
+console.log(`Acessando um item específico (índice [2]) do array de strings: ${arrayString[2]}`)
+console.log(`Acessando um item específico (índice [3]) do array de números: ${arrayNumeros[3]}
+`)
+
+//2 - Modificando um item do array
+
+arrayString[3] = "melancia"
+console.log(`Índice [3] (manga substituído por melancia) do array modificado: ${arrayString}`)
+
+arrayNumeros[2] = 100
+console.log(`Índice [2] (3 substituído por 100) do array modificado: ${arrayNumeros}
+`)
+
+
+//3 - Adicionando um item ao arra com o método .push()
+arrayString.push("mamão")
+console.log(`Adicionando um item ao array de strings: ${arrayString}`)
+
+arrayNumeros.push(5)
+console.log(`Adicionando um item ao array de números: ${arrayNumeros}
+`)
+
+//4 - Removendo um item do array com o pop() - este, irá remover o último item do array
+arrayString.pop()
+console.log(`Removendo o último índice do array de string: ${arrayString} - índice [4] mamão removido`)
+
+arrayNumeros.pop()
+console.log(`Removendo o último índice do array de números: ${arrayNumeros} - índice [4] 5 removido
+`)
+
+//5 - Comparar elementos de um array com outro
+
+console.log(`O ${arrayString[2]} é igual ao ${arrayNumeros[3]}? ${arrayNumeros[2] === arrayString[2]} `)
+console.log(`O ${arrayString[0]} é igual ao ${arrayNumeros[4]} (índice inexistente) ? ${arrayNumeros[4] === arrayString[0]}
+`)
+console.log(`Comparação de índices do mesmo array. ${arrayString[0]} é igual ao ${arrayString[2]}? ${arrayString[0]==arrayString[2]}`)
+
+console.log(`Comparação de índices do mesmo array. ${arrayNumeros[0]} é igual ao ${arrayNumeros[2]}? ${arrayNumeros[0]==arrayNumeros[2]}
+`)
+
+
+
+
+
+
