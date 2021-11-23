1e89bbfab8b19cde339a879ac12d0378cd0b0367
gabazevdo <gbldja@gmail.com>
2021-11-23 18:42:56 -0300

    Iniciando atividade aula 13

---
+++ PROGRAMAÇÃO_IMPERATIVA/aula13/ex01.js
@@ -0,0 +1,33 @@
+/* Exercício de Fixação
+Como sabemos, JSON são objetos literais do JavaScript que nos permitem enviar e receber
+informações entre diferentes tecnologias.
+Algo parecido com <isso:></isso:>
+{
+"nome":"Christopher",
+"profissao":"Programador",
+"idade":25,
+"linguagens":["PHP","Javascript","Dart"],
+"disponibilidadeParaViajar":true,
+"ramoProfissional": {
+"anosDeExperiencia": 12,
+"nivel": "Senior"
+}
+}
+
+Você pensou sobre onde o JSON está localizado?
+Todos os aplicativos/sites que navegamos regularmente têm seu back-end. Se eles nos
+permitem criar nosso usuário, certamente têm um JSON sobre nós.
+
+Qual rede social você acha que pode ser um JSON que tem os seguintes dados?
+● Nome
+● Apelido
+● Idade
+● Usuario
+● Email
+
+Provavelmente podem ser muitos, você pensou em algum específico? Por que você
+considerou isso? Quais dados estariam faltando para tornar a rede social que você achou
+mais completa?
+
+Pedimos agora, para pensar no JSON hipotético que as seguintes redes sociais usam, para
+exibir as informações de um usuário armazenadas no banco de dados no front-end. */
