5f08eec6ac071f47e9d2bcef20f2183fefc2eac2
gabazevdo <gbldja@gmail.com>
2022-05-18 01:41:52 -0300

    Exercício aula 02 concluído

---
+++ Bimestre_3/FrontEnd_II/aula-02/main.js
@@ -0,0 +1,65 @@
+// 1. Obter qual a jogada do jogador
+
+// 2. Gerar aleatoriamente entre 1 e 3
+
+// 3. Calcular os resultados
+
+const joKenPo = ['','Pedra', 'Papel', 'Tesoura']
+let escolhaScript
+let escolhaUsuario
+
+function novoJogo() {
+  escolhaJogadores()
+  opcaoJogada()
+  jogarNovamente()
+}
+
+function escolhaJogadores() {
+  escolhaUsuario = Number(
+    prompt(`
+            VAMOS JOGAR UM JOGO?
+
+            Escolha sua opção
+            1 - Pedra 
+            2 - Papel 
+            3 - Tesoura
+`)
+  )
+  escolhaScript = Math.floor(Math.random() * 3)+1
+}
+
+function opcaoJogada() {
+  if (escolhaUsuario <= joKenPo.length -1) {
+
+    if (escolhaUsuario == 1 && escolhaScript == 2 || escolhaUsuario == 2 && escolhaScript == 3 || escolhaUsuario == 3 && escolhaScript == 1) {
+      alert(`PERDEU
+
+      Você ${joKenPo[escolhaUsuario]}
+      Robo ${joKenPo[escolhaScript]}
+      `)
+    } else if (joKenPo[escolhaScript] == joKenPo[escolhaUsuario]) {
+      alert(`EMPATE
+
+      Você ${joKenPo[escolhaUsuario]}
+      Robo ${joKenPo[escolhaScript]}
+      `)
+    } else {
+      alert(`GANHOU
+
+      Você ${joKenPo[escolhaUsuario]}
+      Robo ${joKenPo[escolhaScript]}
+      `)
+    }
+  } else {
+    alert('Opção inválida, tente novamente')
+    novoJogo()
+  }
+}
+
+function jogarNovamente() {
+  let deNovo = confirm('Deseja tentar novamente?')
+  //if ternário
+  deNovo ? novoJogo(): '' 
+}
+
+novoJogo()
